7f94e14559caeff4361272a628fcca24de9b50b9
bucky0112 <bucky0112@gmail.com>
2023-07-09 02:50:35 +0800

    Fetch and set market titles from API response in Markets component

--- src/components/components/Main/Markets.tsx
+++ src/components/components/Main/Markets.tsx
@@ -1,9 +1,47 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { Stack, Text } from 'native-base'
+import { setTitles, AppDispatch } from '@Stores/index'
+import { MARKETS_URL } from '@Api/index'
+import { Headers } from "../Markets"
 
-const Markets = () => (
-  <Stack>
-    <Text>Markets</Text>
-  </Stack>
-)
+const Markets = () => {
+  const dispatch = useDispatch<AppDispatch>()
+  const [selectedHeader, setSelectedHeader] = useState<string>("BTC")
+
+  const fetchMarkets = async () => {
+    try {
+      const headers = {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json;charset=utf-8',
+        'user-agent': 'Android;1.15.0',
+        'TOK-DEVICE-ID': 'ea278b7741967a5e'
+      }
+
+      const response = await fetch(MARKETS_URL, { method: 'GET', headers })
+
+      if (!response.ok) {
+        throw new Error('Login failed')
+      }
+
+      const data = await response.json()
+      const titles = data?.data?.map((item: any) => item?.title)
+      dispatch(setTitles(titles))
+    } catch (error) {
+      console.error('Failed to login:', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchMarkets()
+  }, [])
+
+  return (
+    <Stack>
+      <Headers selected={selectedHeader} atClick={setSelectedHeader} />
+      <Text>Markets</Text>
+    </Stack>
+  )
+}
 
 export default Markets
---
+++ src/components/components/Markets/Headers.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react'
+import { useSelector } from 'react-redux'
+import { HStack, Pressable, Text, Center } from 'native-base'
+import { RootState } from '@Stores/index'
+
+interface HeadersProps {
+  selected: string
+  atClick: (selected: string) => void
+}
+
+const Headers: FC<HeadersProps> = ({ selected, atClick }) => {
+  const titles = useSelector((state: RootState) => state.market.titles)
+
+  return (
+    <HStack px="3" justifyContent="space-between">
+      {titles?.map((title: string) => (
+        <Pressable
+          w={`${(100 / titles.length) - 2}%`}
+          py="2"
+          rounded='6'
+          key={title}
+          bg={selected === title ? 'buttonBlue.600' : 'buttonBlue.500'}
+          _pressed={{ bg: 'buttonBlue.600' }}
+          onPress={() => atClick(title)}
+        >
+          <Center>
+            <Text >{title}</Text>
+          </Center>
+        </Pressable>
+      ))}
+    </HStack>
+  )
+}
+
+export default Headers
