2cec44d61f31b8ef5368a99830a9162fb9f000ec
bucky0112 <bucky0112@gmail.com>
2023-07-08 16:10:39 +0800

    Update babel.config.js to include a module resolver plugin and rename components/NativeBaseIcon.tsx to src/components/components/NativeBaseIcon.tsx. Also, update tsconfig.json with additional paths for navigation, components, screens, stores, and assets

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@Components': './src/components',
+            '@Screens': './src/screens',
+            '@Stores': './src/stores',
+            '@Assets': './assets'
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx']
+        }
+      ]
+    ]
+  }
+}
