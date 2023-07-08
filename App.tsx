f15ebd36848d15583d06dd3590f4ed1cade75e77
bucky0112 <bucky0112@gmail.com>
2023-07-08 18:24:58 +0800

    Refactor App.tsx to import the Provider and store from 'react-redux' and '@Stores/index', and update the JSX structure to wrap the Login component with the Provider component from react-redux

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React from 'react'
+import { Provider } from 'react-redux'
+import store from '@Stores/index'
 import {
   Text,
   Link,
@@ -10,45 +12,47 @@ import {
   NativeBaseProvider,
   extendTheme,
   VStack,
-  Box,
-} from "native-base";
-import NativeBaseIcon from "@Components/components/NativeBaseIcon";
-import Login from "@Screens/Login"
+  Box
+} from 'native-base'
+import NativeBaseIcon from '@Components/components/NativeBaseIcon'
+import Login from '@Screens/Login'
 
 // Define the config
 const config = {
   useSystemColorMode: false,
-  initialColorMode: "dark",
-};
+  initialColorMode: 'dark'
+}
 
 // extend the theme
-export const theme = extendTheme({ config });
-type MyThemeType = typeof theme;
-declare module "native-base" {
+export const theme = extendTheme({ config })
+type MyThemeType = typeof theme
+declare module 'native-base' {
   interface ICustomTheme extends MyThemeType {}
 }
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Login />
-    </NativeBaseProvider>
-  );
+    <Provider store={store}>
+      <NativeBaseProvider>
+        <Login />
+      </NativeBaseProvider>
+    </Provider>
+  )
 }
 
 // Color Switch Component
 function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode()
   return (
-    <HStack space={2} alignItems="center">
+    <HStack space={2} alignItems='center'>
       <Text>Dark</Text>
       <Switch
-        isChecked={colorMode === "light"}
+        isChecked={colorMode === 'light'}
         onToggle={toggleColorMode}
         aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
+          colorMode === 'light' ? 'switch to dark mode' : 'switch to light mode'
         }
       />
       <Text>Light</Text>
     </HStack>
-  );
+  )
 }
