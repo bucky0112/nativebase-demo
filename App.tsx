d2647f00909ee49b6a75fe393809639b5b9acd18
bucky0112 <bucky0112@gmail.com>
2023-07-08 19:19:23 +0800

    Refactor App.tsx to use the Root component instead of the Login component

--- App.tsx
+++ App.tsx
@@ -1,21 +1,11 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import store from '@Stores/index'
 import {
-  Text,
-  Link,
-  HStack,
-  Center,
-  Heading,
-  Switch,
-  useColorMode,
   NativeBaseProvider,
   extendTheme,
-  VStack,
-  Box
 } from 'native-base'
-import NativeBaseIcon from '@Components/components/NativeBaseIcon'
-import Login from '@Screens/Login'
+import store from '@Stores/index'
+import Root from '@Components/Root'
 
 // Define the config
 const config = {
@@ -33,26 +23,8 @@ export default function App() {
   return (
     <Provider store={store}>
       <NativeBaseProvider>
-        <Login />
+        <Root />
       </NativeBaseProvider>
     </Provider>
-  )
-}
-
-// Color Switch Component
-function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode()
-  return (
-    <HStack space={2} alignItems='center'>
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === 'light'}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === 'light' ? 'switch to dark mode' : 'switch to light mode'
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
   )
 }
---
+++ src/components/Root.tsx
@@ -0,0 +1,12 @@
+import { useSelector } from 'react-redux'
+import { RootState } from '@Stores/index'
+import Login from '@Screens/Login'
+import Main from '@Screens/Main'
+
+const Root = () => {
+  const token = useSelector((state: RootState) => state.auth.token)
+
+  return token ? <Main /> : <Login />
+}
+
+export default Root
