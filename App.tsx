0400476ec4afef95408c35cdef4dcfa9a6767a85
bucky0112 <bucky0112@gmail.com>
2023-07-09 15:28:18 +0800

    Refactor the import statements and add new color variables to the color theme in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import {
-  NativeBaseProvider,
-  extendTheme,
-} from 'native-base'
+import { NativeBaseProvider, extendTheme } from 'native-base'
 import store from '@Stores/index'
 import Root from '@Components/Root'
 
 const newColorTheme = {
   buttonBlue: {
     500: '#e4e9f9',
-    600: '#6992ff',
+    600: '#6992ff'
   },
   mainBackground: '#f7f8fd',
-};
+  mainText: '#3d436c',
+  subText: '#8e92b2'
+}
 
 // extend the theme
 export const theme = extendTheme({ colors: newColorTheme })
